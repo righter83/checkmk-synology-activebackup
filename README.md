ce92fa2416bcff5a604d8060b77d0573a7486ef3
miri <michael.richter@star-group.net>
2022-07-22 14:42:13 +0200

    initial

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # checkmk-synology-activebackup
+## Info
+
+This scripts is written for CheckMK.
+It uses the SQLite Databases to get the job results for the Synology Active Backups.
+Tested on:
+- CheckMK 2.0
+- Synology DSM 7.1
+- Active Backup for Business 2.4.1
+
+## Install
+* Copy the scripty to your Synology NAS and make it excecutable.
+* Install the SSH Key from CheckMK on your NAS.
+* Afterwards you have to configure the Check in CheckMK:
+Go to Services -> Other Services and add a "Check via SSH service"
+Configure everything for your NAS and the command should be like:
+```/volume1/check_ab.php```
+
+## Config modifications
+There is also a check inside which checks if the job was running in a specific time window.
+If you run a job daily you should adjust the config variable runtime to 86500 -> a little bit higher as one day.
+You can also disable this check if you set the runtimecheck variable to False
+
+## Outputs
+![](https://github.com/righter83/checkmk-synology-activebackup/images/ok.png)
+
+![](https://github.com/righter83/checkmk-synology-activebackup/images/error.png)
---
+++ check_ab.php
@@ -0,0 +1,61 @@
+#!/bin/php
+<?php
+
+// open DBs
+$dbt=new SQLite3("/volume1/@ActiveBackup/config.db");
+$dbj=new SQLite3("/volume1/@ActiveBackup/activity.db");
+
+// configs
+$runtime=86500;
+$error=0;
+$runtimehuman=gmdate("H:i:s", $runtime);
+$runtimecheck=True;
+$tsnow=Time();
+$now=date("d.m.Y H:m", $tsnow);
+
+// get configure tasks
+$task=$dbt->query("select task_id,task_name from task_table");
+while($tasks=$task->fetchArray())
+{
+	// get last job of each task
+	$job=$dbj->query("select * from result_table where task_id=$tasks[task_id] order by result_id desc limit 1");
+	$jobs=$job->fetchArray();
+	$start=date("d.m.Y H:m", $jobs['time_start']);
+	
+
+	// check if job started < runtime
+	if (($tsnow-$runtime) > $jobs['time_start'] && $runtimecheck)
+	{
+		$out.="ERROR: $jobs[task_name] was not running inside runtime window (Now: $now, Last Start: $start, Not older as:. $runtimehuman ) -- ";
+		$error=2;
+	} 
+
+	// check if job is running
+	if ($jobs['status'] == 1)
+	{
+		$out.="OK: $jobs[task_name] is running -- ";
+		continue;
+	}		
+
+	//warnings
+	if ($jobs['status'] == 3)
+	{
+		$out.="WARN: $jobs[task_name] had Warnings -- ";
+		$error=1;
+	}
+	
+	// errors
+	if ($jobs['error_count'] > 0)
+        {
+                $out.="ERROR: $jobs[task_name] had an Error -- ";
+                $error=2;
+        }
+	if ($error == 0)
+	{
+		$out.="OK: $jobs[task_name] runned successfully -- ";
+	}
+
+}
+
+echo $out;
+exit($error);
